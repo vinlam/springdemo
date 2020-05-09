2c61e026bfbed8cc817834300076274c6bb1ae26
vinlam <zwlvin@163.com>
2020-05-09 16:00:43 +0800

    moidfied

--- LogAspect/src/main/java/com/controller/web/rest/RestApiTestController.java
+++ LogAspect/src/main/java/com/controller/web/rest/RestApiTestController.java
@@ -41,12 +41,16 @@ import com.define.annotation.CacheLock;
 import com.define.annotation.CacheParam;
 import com.define.annotation.LocalLock;
 import com.entity.Person;
+import com.entity.SafeConfess;
+import com.entity.SafeConfessContent;
 import com.entity.TestDTO;
 import com.entity.User;
 import com.entity.UserDTO;
+import com.google.common.collect.Maps;
 import com.service.GreetingService;
 import com.service.IAutoInject;
 import com.service.SaveDataService;
+import com.util.ExportExcelByPoiUtil;
 import com.util.JsonMapper;
 
 import io.swagger.annotations.ApiImplicitParam;
@@ -289,5 +293,153 @@ public class RestApiTestController {
     @CacheLock(prefix = "redisLock.test",expire = 20)
     public String cacheLock2(@CacheParam(name = "token") String token){
         return "sucess====="+token;
+    }
+    //http://localhost:8091/LogAspect/api/exportSafeConfess
+    @RequestMapping(value = "/exportSafeConfess", method = RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+    public void exportSafeConfess(HttpServletRequest request, HttpServletResponse response) {
+        // 获取页面时间参数
+        List<SafeConfess> resultList = new ArrayList<SafeConfess>();
+        
+        List<SafeConfessContent> list =  new ArrayList<SafeConfessContent>();
+        SafeConfessContent confessContent = new SafeConfessContent();
+        confessContent.setcCheckResult("0");
+        confessContent.setcContent("c01");
+        confessContent.setcStatus("OK");
+        SafeConfessContent confessContent1 = new SafeConfessContent();
+        confessContent1.setcCheckResult("1");
+        confessContent1.setcContent("c02");
+        confessContent1.setcStatus("OK");
+        SafeConfessContent confessContent2 = new SafeConfessContent();
+        confessContent2.setcCheckResult("0");
+        confessContent2.setcContent("c03");
+        confessContent2.setcStatus("Fail");
+        list.add(confessContent);
+        list.add(confessContent1);
+        list.add(confessContent2);
+        SafeConfess confess = new SafeConfess();
+        confess.setComfirmCheckPerson("CCP");
+        confess.setComfirmCheckTime("2020-05-09");
+        confess.setCompanyPerson("CP");
+        confess.setCompanyPhone("13800138001");
+        confess.setDangerDiscern("DD");
+        confess.setDangerMeasures("DM");
+        confess.setDangerManager("DMR");
+        confess.setDeptName("DN");
+        confess.setEnvironmentDiscren("ED");
+        confess.setEnvironmentMeasures("EM");
+        confess.setFactoryPerson("FP");
+        confess.setFactoryPhone("020-8888888");
+        confess.setManagerTime("2020-05-09");
+        confess.setProjectName("PN");
+        confess.setProjectNumber("P01");
+        confess.setProjectPerson("PP");
+        confess.setSafeConfessContentList(list);
+        confess.setSafeName("SN");
+        confess.setSceneComfirmPer("SCP");
+        confess.setTestAfterPerson("TAP");
+        confess.setTestAfterTime("2020-05-10");
+        confess.setTestBeforePerson("TBP");
+        confess.setTestBeforeTime("2020-05-15");
+        confess.setTestPerson("TP");
+        confess.setTestTime("2020-05-09");
+        confess.setWorkAddress("WAGD");
+        confess.setWorkNumber("WN01");
+        
+        SafeConfess confess1 = new SafeConfess();
+        confess1.setComfirmCheckPerson("CCP1");
+        confess1.setComfirmCheckTime("2020-05-09");
+        confess1.setCompanyPerson("CP");
+        confess1.setCompanyPhone("13800138002");
+        confess1.setDangerDiscern("DD");
+        confess1.setDangerMeasures("DM");
+        confess1.setDangerManager("DMR");
+        confess1.setDeptName("DN");
+        confess1.setEnvironmentDiscren("ED");
+        confess1.setEnvironmentMeasures("EM");
+        confess1.setFactoryPerson("FP");
+        confess1.setFactoryPhone("020-8888888");
+        confess1.setManagerTime("2020-05-09");
+        confess1.setProjectName("PN");
+        confess1.setProjectNumber("P01");
+        confess1.setProjectPerson("PP");
+        confess1.setSafeConfessContentList(list);
+        confess1.setSafeName("SN");
+        confess1.setSceneComfirmPer("SCP");
+        confess1.setTestAfterPerson("TAP");
+        confess1.setTestAfterTime("2020-05-10");
+        confess1.setTestBeforePerson("TBP");
+        confess1.setTestBeforeTime("2020-05-15");
+        confess1.setTestPerson("TP");
+        confess1.setTestTime("2020-05-09");
+        confess1.setWorkAddress("WAGD");
+        confess1.setWorkNumber("WN01");
+        
+        String titleAttr[] = { "安全交底名称", "部门名称", "项目名称", "项目编号", "项目负责人", "工作地点", "工作编号", "公司联系人", "公司联系方式", "厂方联系人",
+                "厂方联系方式", "现场确认人员 ", "执行人", "执行时间", "现场试验其它危险源辨识", "针对上述危险源制定的安全措施", "现场试验其它环境因素识别", "针对上述环境因素的控制措施",
+                "措施确认检查人", "措施确认检查时间", "控制措施内容", "控制措施结果", "技术措施内容", "技术措施结果", "试验前检查人", "试验前检查时间", "试验前检查内容",
+                "试验前检查结果", "试验中检查人", "试验中检查时间", "试验中检查内容", "试验中检查结果", "试验后检查人", "试验后检查时间", "试验后检查内容", "试验后检查结果" };
+        int widthAttr[] = { 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30,
+                30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30 };
+        String titleHead = "安全交底";
+        List<Map<String, String>> dataList = new ArrayList<Map<String, String>>();
+        for (SafeConfess safeConfess : resultList) {
+            Map<String, String> temp = null;
+            List<SafeConfessContent> safeConfessContentList = safeConfess.getSafeConfessContentList();
+            if (safeConfessContentList.size() > 0) {
+                for (SafeConfessContent safeConfessContent : safeConfessContentList) {
+                    temp = new HashMap<String, String>();
+                    temp.put("安全交底名称", safeConfess.getSafeName());
+                    temp.put("部门名称", safeConfess.getDeptName());
+                    temp.put("项目名称", safeConfess.getProjectName());
+                    temp.put("项目编号", safeConfess.getProjectNumber());
+                    temp.put("项目负责人", safeConfess.getProjectPerson());
+                    temp.put("工作地点", safeConfess.getWorkAddress());
+                    temp.put("工作编号", safeConfess.getWorkNumber());
+                    temp.put("公司联系人", safeConfess.getCompanyPerson());
+                    temp.put("公司联系方式", safeConfess.getCompanyPhone());
+                    temp.put("厂方联系人", safeConfess.getFactoryPerson());
+                    temp.put("厂方联系方式", safeConfess.getFactoryPhone());
+                    temp.put("现场确认人员", safeConfess.getSceneComfirmPer());
+                    temp.put("执行人", safeConfess.getDangerManager());
+                    temp.put("执行时间", safeConfess.getManagerTime());
+                    temp.put("现场试验其它危险源辨识", safeConfess.getDangerDiscern());
+                    temp.put("针对上述危险源制定的安全措施", safeConfess.getDangerMeasures());
+                    temp.put("现场试验其它环境因素识别", safeConfess.getEnvironmentDiscren());
+                    temp.put("针对上述环境因素的控制措施", safeConfess.getEnvironmentMeasures());
+                    temp.put("措施确认检查人", safeConfess.getComfirmCheckPerson());
+                    temp.put("措施确认检查时间", safeConfess.getComfirmCheckTime());
+                    // 状态0：安全及环境相关控制措施 1：技术措施 2：试验前 3：试验中 4：试验后
+                    if (safeConfessContent.getcStatus().equals("0")) {
+                        temp.put("控制措施内容", safeConfessContent.getcContent());
+                        temp.put("控制措施结果", safeConfessContent.getcCheckResult());
+
+                    } else if (safeConfessContent.getcStatus().equals("1")) {
+                        temp.put("技术措施内容", safeConfessContent.getcContent());
+                        temp.put("技术措施结果", safeConfessContent.getcCheckResult());
+                    } else if (safeConfessContent.getcStatus().equals("2")) {
+                        temp.put("试验前检查人", safeConfess.getTestBeforePerson());
+                        temp.put("试验前检查时间", safeConfess.getTestBeforeTime());
+                        temp.put("试验前检查内容", safeConfessContent.getcContent());
+                        temp.put("试验前检查结果", safeConfessContent.getcCheckResult());
+                    } else if (safeConfessContent.getcStatus().equals("3")) {
+                        temp.put("试验中检查人", safeConfess.getTestPerson());
+                        temp.put("试验中检查时间", safeConfess.getTestTime());
+                        temp.put("试验中检查内容", safeConfessContent.getcContent());
+                        temp.put("试验中检查结果", safeConfessContent.getcCheckResult());
+                    } else if (safeConfessContent.getcStatus().equals("4")) {
+                        temp.put("试验后检查人", safeConfess.getTestAfterPerson());
+                        temp.put("试验后检查时间", safeConfess.getTestAfterTime());
+                        temp.put("试验后检查内容", safeConfessContent.getcContent());
+                        temp.put("试验后检查结果", safeConfessContent.getcCheckResult());
+                    }
+                    dataList.add(temp);
+                }
+            } else {
+                temp = new HashMap<String, String>();
+            }
+        }/* 此处的key为每个sheet的名称，一个excel中可能有多个sheet页 *//* 此处key对应每一列的标题 *//* 该list为每个sheet页的数据 */
+        Map<String, List<Map<String, String>>> map = Maps.newHashMap();
+        map.put("安全交底列表", dataList);
+        ExportExcelByPoiUtil.createExcel(request, response, titleAttr, titleHead, widthAttr, map, new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 }/* 此处数组为需要合并的列，可能有的需求是只需要某些列里面相同内容合并 */);
     }
 }
